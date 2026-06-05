16ae3eaf192aa3ff10582bc2375be00d93cd6ef9
tecrade <jovinjohn.developer@gmail.com>
2026-06-05 19:29:16 +0530

    responsive issue of banner for mobile devices fixed

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -10,41 +10,45 @@ const Banner = () => {
   }, []);
 
   return (
-    <div className="w-full min-h-screen ">
+    <div className="w-full min-h-screen">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col md:flex-row items-center">
           <hr className="border-[var(--light)] opacity-20" />
-          
+
           {/* Left side - Name and Title */}
           <div className="w-full md:w-1/2">
-            <div 
-              className="h-[300px] w-auto border-2 border-transparent text-center mx-auto my-24 leading-[5rem] text-[var(--light)]"
-              data-aos="fade-right" 
+            <div
+              className="w-auto border-2 border-transparent text-center mx-auto mt-12 mb-8 md:my-24 leading-[3rem] md:leading-[5rem] text-[var(--light)]"
+              data-aos="fade-right"
               data-aos-duration="1500"
             >
-              <h1 className="mt-16 text-[60px] tracking-[0.7rem] font-normal">JOVIN JOHN</h1>
-              <div className="mt-8 tracking-[0.5rem] text-[24px]">
-                <h3 className="text-[var(--light)]">I'M A <Typer /></h3>
+              <h1 className="mt-8 md:mt-16 text-[36px] sm:text-[48px] md:text-[60px] tracking-[0.3rem] sm:tracking-[0.5rem] md:tracking-[0.7rem] font-normal">
+                JOVIN JOHN
+              </h1>
+              <div className="mt-4 md:mt-8 tracking-[0.3rem] sm:tracking-[0.5rem] text-[16px] sm:text-[20px] md:text-[24px]">
+                <h3 className="text-[var(--light)] whitespace-nowrap">I'M A <Typer /></h3>
               </div>
-              <Link
-                to="/contact"
-                className="relative inline-flex items-center justify-center h-12 w-auto border-2 border-[var(--blue)] text-[var(--blue)] rounded-[0.3rem] z-[1] shadow-[0px_5px_15px_rgba(9,156,242,0.1),0px_10px_20px_rgba(0,91,234,0.1)] hover:text-[var(--light)] px-6 after:content-[''] after:absolute after:h-full after:w-[30%] after:bg-transparent after:top-0 after:left-0 after:right-0 after:-z-[1] hover:after:w-full hover:after:bg-[var(--blue)] hover:after:transition-[width_0.5s]"
-              >
-                Connect me
-              </Link>
+              <div className="mt-6 md:mt-8">
+                <Link
+                  to="/contact"
+                  className="relative inline-flex items-center justify-center h-12 w-auto border-2 border-[var(--blue)] text-[var(--blue)] rounded-[0.3rem] z-[1] shadow-[0px_5px_15px_rgba(9,156,242,0.1),0px_10px_20px_rgba(0,91,234,0.1)] hover:text-[var(--light)] px-6 after:content-[''] after:absolute after:h-full after:w-[30%] after:bg-transparent after:top-0 after:left-0 after:right-0 after:-z-[1] hover:after:w-full hover:after:bg-[var(--blue)] hover:after:transition-[width_0.5s]"
+                >
+                  Connect me
+                </Link>
+              </div>
             </div>
           </div>
 
           {/* Right side - Picture */}
           <div className="w-full md:w-1/2">
-            <div 
-              className="h-[500px] w-[500px] border-0 border-[var(--light)] mx-auto my-8 overflow-hidden rounded-lg"
-              data-aos="zoom-in-left" 
+            <div
+              className="h-[260px] w-[260px] sm:h-[360px] sm:w-[360px] md:h-[500px] md:w-[500px] border-0 border-[var(--light)] mx-auto mb-10 md:my-8 overflow-hidden rounded-lg"
+              data-aos="zoom-in-left"
               data-aos-duration="1500"
             >
-              <img 
-                src="/images/profilepic2.png" 
-                alt="Jovin John" 
+              <img
+                src="/images/profilepic2.png"
+                alt="Jovin John"
                 className="w-full h-full object-cover"
               />
             </div>
@@ -56,4 +60,4 @@ const Banner = () => {
   );
 };
 
-export default Banner; 
+export default Banner;
